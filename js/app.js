60fc087ddc35cf7dfeb626b829b4b6b311c56106
Alejo Rondon <blackphotografy@gmail.com>
2020-06-20 15:11:47 -0500

    [EDIT] JS > scrollreveal

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ let menuButton = document.getElementsByClassName('menu-btn')[0]
 menuButton.addEventListener('click', () => {
   document.querySelector('.nav-menu').classList.toggle('show')
 })
+
+ScrollReveal().reveal('.showcase') //, { delay: 500 })
+ScrollReveal().reveal('.news-cards', { delay: 500 })
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 })
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 })
